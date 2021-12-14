82536739b831282e6d02bee3ef07b75b1feaeb24
Ryan L <95482464+ryanl1993@users.noreply.github.com>
2021-12-14 18:32:24 +0000

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,14 @@
+const auth = {
+
+  //API Key Below for Torn Purposes
+  apiKey: '',
+
+  // One API Key per faction, for faction-specific features.
+  // Use meaningful keynames for easy reference. e.g. `chain faction1`
+  factionKeys: {
+    faction1: '',
+    faction2: '',
+  }
+};
+
+module.exports = auth;
